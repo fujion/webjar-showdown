daad748d34182378ac5cd0f500674a24f05d31fb
Douglas Martin <mdgeek1@gmail.com>
2023-10-14 10:14:43 -0400

    POM refactoring.

--- staging/gulpfile.js
+++ staging/gulpfile.js
@@ -1,6 +1,6 @@
 const {src, dest, series} = require('gulp');
 
-const srcDir = '${src.rootdir}/node_modules/showdown/';
+const srcDir = '${webjar.staging}/node_modules/showdown/';
 const destDir = '${webjar.target}/';
 
 function task1() {
